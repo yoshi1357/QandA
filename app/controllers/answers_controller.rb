62e63426c9724601b8584837312bda4fc9fed76b
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-04 06:55:01 +0900

    first commit

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,21 @@
+class AnswersController < ApplicationController
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+
+    if @answer.update(answer_params)
+      redirect_to question_path(@question),notice: 'Success!'
+    else
+      redirect_to question_path(@question),alert: 'invalid!'
+    end
+  end
+
+  def edit
+  end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:content, :name, :question_id)
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1,name: 'Test Name1', title: 'Test Title1', content: 'Test Content1')
+Question.create(id: 2,name: 'Test Name2', title: 'Test Title2', content: 'Test Content2')
+Question.create(id: 3,name: 'Test Name3', title: 'Test Title3', content: 'Test Content3')
