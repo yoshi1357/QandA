c75edf32f46e8521e40cd2d50becdfe11cdc4fdd
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-09 21:35:26 +0900

    refactoring apllication.scss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
+
   def set_title(page_title = "")
     base_title = "わいわいみんなのQ&Aサイト!"
     page_title.empty? ? base_title : page_title+" | "+base_title
+  end
+
+  def simple_time(japan_time)
+    japan_time.strftime("%Y-%m-%d %H:%M")
   end
 end
