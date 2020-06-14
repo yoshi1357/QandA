f93bac6d29f50542d5a0c3fe7392b1188f1522f8
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-14 16:55:32 +0900

    refactoring show

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -46,7 +46,7 @@ document.addEventListener("turbolinks:load", function() {
 
     {// show
       $("#show button").click(function(){
-        $(this).parent().next().toggleClass('disable')
+        $(this).next().toggleClass('disable')
 
         if ($(this).html() === "この質問に回答する！") {
           $(this).html("回答フォームを折りたたむ");
