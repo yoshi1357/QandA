f9ef7dc6d73de5aa2f6ee7bc92cac1bd1d78a795
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-16 22:14:17 +0900

    Set rooting & Start change model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # get 'questions/index'
   # get 'questions/show'
   # get 'questions/new'
   # get 'questions/edit'
 
   root 'questions#index'
-  resources :questions do
-    resources :answers
+  # devise_for :users, :controllers => {
+  #   :registrations => 'users/registrations',
+  #   :sessions => 'users/sessions',
+  #   :passwords => 'users/passwords'
+  # }
+
+  devise_for :users
+
+  resources :users do
+    # member do
+    #   get 'questins/new'
+    #   get 'answers/new'
+    # end
+    resources :questions, only: [:new, :create, :edit, :update, :destroy]
+    resources :answers, only:  [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :questions, only: [:index, :show] do
+    resources :answers, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_141256) do
+ActiveRecord::Schema.define(version: 2020_06_16_115445) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_141256) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
