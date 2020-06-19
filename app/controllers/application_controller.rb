cee7059449db177ae8e2958b9b4bb4a0c31e69c0
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-19 11:16:45 +0900

    Imp user-image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+ 	  devise_parameter_sanitizer.permit(:sign_up, keys: [:image]) # 新規登録時(sign_up時)にimageというキーのパラメーターを追加で許可する
   end
+
 end
