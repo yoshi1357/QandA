fee6071500fc2b9fa460e5db87e74e6da5c63dca
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-06-24 01:45:22 +0900

    finish 6/24

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :set_latest_questions, :set_nores_questions, :set_nores
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_questions = @user.questions
+    @user_answers = @user.answers
+  end
+
+
+  private
+
+
+end
