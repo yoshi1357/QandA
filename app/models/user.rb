ea907d4dbe6bc932fc46027229545ecf37e38a7d
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-07-10 14:16:56 +0900

    Imp ValidationTest

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/
 
-  validates :name, { presence: true, uniqueness: { case_sensitive: false } }
+  validates :name, { presence: true, length: {maximum: 10}, uniqueness: true }
   validates :email, { presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX } }
   #半角英小文字大文字数字をそれぞれ1種類以上含む8文字以上100文字以下
-  validates :password, { format: { with: VALID_PASSWORD_REGEX ,
+  validates :password, { presence: true, format: { with: VALID_PASSWORD_REGEX ,
   message: "半角英小文字、大文字、数字をそれぞれ1種類以上含む8文字以上100文字以下の必要があります" }, confirmation: true }
 
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+RSpec.describe "UserValidation", type: :model do
+
+  describe 'is valid weth a name, email, password' do
+    it 'name, email, passwordがあれば有効なこと' do
+      user = User.new(
+        name: 'John',
+        email: 'abcde@email.com',
+        password: 'Password1'
+      )
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'name' do
+
+    describe 'presence: true' do
+      it 'nameがblankの場合エラーになる' do
+        user = User.new(name: '')
+        user.valid?
+        expect(user.errors[:name]).to include('を入力してください')
+      end
+    end
+    describe 'length: {maximum: 10}' do
+      it 'nameが11文字以上の場合エラーになる' do
+        user = User.new(name: 'とりあえずテストで期待')
+        user.valid?
+        expect(user.errors[:name]).to include('は10文字以内で入力してください')
+      end
+    end
+
+    describe 'uniqueness: true' do
+      it 'nameが同じ既にある名前ならエラーになる' do
+        User.create!(name: 'nakatsuka', email: 'test1@email.com', password: "Password1")
+        user = User.new(name: 'nakatsuka', email: 'test2@email.com', password: "Password2")
+        user.valid?
+        expect(user.errors[:name]).to include('はすでに存在します')
+      end
+    end
+  end
+
+  describe 'email' do
+    describe 'presence: true' do
+      it 'emailがblankの場合エラーになる' do
+        user = User.new(email: '')
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+      end
+    end
+    describe 'uniqueness: { case_sensitive: false }' do
+      it 'nameが同じ既にある名前ならエラーになる(大文字小文字区別しない)' do
+        User.create!(name: 'nakatsuka1', email: 'test@email.com', password: "Password1")
+        user = User.new(name: 'nakatsuka2', email: 'TEST@email.com', password: "Password2")
+        user.valid?
+        expect(user.errors[:email]).to include('はすでに存在します')
+      end
+    end
+    describe 'format: { with: VALID_EMAIL_REGEX }' do
+      it '正規表現にそぐわないとエラーになる（@なし）' do
+        user = User.new(email: 'testemail.com')
+        user.valid?
+        expect(user.errors[:email]).to include('は不正な値です')
+      end
+    end
+  end
+
+  describe 'password' do
+    describe 'presence: true' do
+      it 'passwordがblankの場合エラーになる' do
+        user = User.new(password: '')
+        user.valid?
+        expect(user.errors[:password]).to include('を入力してください')
+      end
+    end
+    describe 'format: { with: VALID_EMAIL_REGEX }' do
+      it '正規表現にそぐわないとエラーになる（8文字未満）' do
+        user = User.new(password: 'Pass1')
+        user.valid?
+        expect(user.errors[:password]).to include('半角英小文字、大文字、数字をそれぞれ1種類以上含む8文字以上100文字以下の必要があります')
+      end
+    end
+    describe 'format: { with: VALID_EMAIL_REGEX }' do
+      it '正規表現にそぐわないとエラーになる（英大文字なし）' do
+        user = User.new(password: 'password1')
+        user.valid?
+        expect(user.errors[:password]).to include('半角英小文字、大文字、数字をそれぞれ1種類以上含む8文字以上100文字以下の必要があります')
+      end
+    end
+    describe 'format: { with: VALID_EMAIL_REGEX }' do
+      it '正規表現にそぐわないとエラーになる（英小文字なし）' do
+        user = User.new(password: 'PASSWORD1')
+        user.valid?
+        expect(user.errors[:password]).to include('半角英小文字、大文字、数字をそれぞれ1種類以上含む8文字以上100文字以下の必要があります')
+      end
+    end
+    describe 'format: { with: VALID_EMAIL_REGEX }' do
+      it '正規表現にそぐわないとエラーになる（数字なし）' do
+        user = User.new(password: 'Password')
+        user.valid?
+        expect(user.errors[:password]).to include('半角英小文字、大文字、数字をそれぞれ1種類以上含む8文字以上100文字以下の必要があります')
+      end
+    end
+
+  end
+end
