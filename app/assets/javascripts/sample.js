9de66d19e4dc701f1ae024c3cd4453b7390d26ed
EC2 Default User <ec2-user@ip-172-31-46-34.ap-northeast-1.compute.internal>
2020-07-14 00:48:57 +0900

    Imp Preview&FixTitle

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -25,17 +25,6 @@ document.addEventListener("turbolinks:load", function() {
 
     {//index
 
-      // マウスオーバーで質問内容を表示
-      // $(".question-title").hide();
-
-      // $(".question-title-abr").hover(function(){
-      //   $(this).hide();
-      //   $(this).next().stop().fadeIn(500);
-      // },function(){
-      //   $(this).next().hide();
-      // 　$(this).next().stop().fadeIn(500);
-      // });
-
       // sort_linkへのボタン化
       $("#index block > a").each(function(){
         if (!$(this).hasClass("btn btn-primary")) {
@@ -43,22 +32,21 @@ document.addEventListener("turbolinks:load", function() {
         }
       });
 
-      //タイトルの省略をホバーで解除
-      // $(".question-title-abr").hover(function(){
-      //   let q_id = $(this).attr('data_qid')
-      //   let question = gon.questions[q_id - 1]
+      // タイトルの省略をホバーで解除
+      $(".question-title-abr").mouseenter(function(){
+        let qid = $(this).attr('data_qid')
 
-      //   $.ajax({
-      //   type: "GET",
-      //   url: "/",
-      //   cache: false,
-      //   //必要なステータスを送るremote:trueを忘れずに
-      //   data: { question: question, remote: true }
-      //   });
+        $.ajax({
+        type: "GET",
+        url: "/",
+        cache: false,
+        dataType: "script",
+        //必要なステータスを送るremote:trueを忘れずに
+        // この値をコントローラー側でparams[:qid]で受け取れる
+        data: { qid: qid, remote: true }
+        });
 
-      // },function(){
-      //   // ホバーアウト時の処理
-      // });
+      });
 
 
 
@@ -106,6 +94,46 @@ document.addEventListener("turbolinks:load", function() {
           $(".pw-form").attr('type','password');
         }
       });
+
+    }
+
+    {//user-edit
+    function buildHTML(image) {
+      var html =
+        `
+        <div class="prev-content">
+          <img src="${image}", alt="preview" class="prev-image rounded d-block mx-auto", width="100", height="150">
+        </div>
+        `;
+      return html;
+    }
+
+    // 画像が選択された時に発火します
+    $(document).on('change', '.hidden_file', function () {
+      // .file_filedからデータを取得して変数fileに代入します
+      var file = this.files[0];
+      // FileReaderオブジェクトを作成します
+      var reader = new FileReader();
+      // DataURIScheme文字列を取得します
+      reader.readAsDataURL(file);
+      // 読み込みが完了したら処理が実行されます
+      reader.onload = function () {
+        // 読み込んだファイルの内容を取得して変数imageに代入します
+        var image = this.result;
+        // プレビュー画像がなければ処理を実行します
+        if ($('.prev-content').length == 0) {
+          // 読み込んだ画像ファイルをbuildHTMLに渡します
+          var html = buildHTML(image);
+          // 作成した.prev-contentをiconの代わりに表示させます
+          $('.prev-contents').prepend(html);
+          // 画像が表示されるのでiconを隠します
+          $('.photo-icon').hide();
+        } else {
+          // もし既に画像がプレビューされていれば画像データのみを入れ替えます
+          $('.prev-content .prev-image').attr({ src: image });
+        }
+      };
+    });
 
     }
 
